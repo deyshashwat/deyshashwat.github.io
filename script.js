842d980bb1fc834538e9ce99f3133b4a7def3b23
deyshashwat <deyshashwat01@gmail.com>
2025-03-31 18:15:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Smooth scroll to sections
+
+    // Smooth Scroll to Sections on Clicking the Navigation Link
     const links = document.querySelectorAll('a[href^="#"]');
     links.forEach(link => {
         link.addEventListener('click', function (e) {
             e.preventDefault();
             const targetId = this.getAttribute('href').slice(1);
             document.getElementById(targetId).scrollIntoView({
-                behavior: 'smooth'
+                behavior: 'smooth',
+                block: 'start'
             });
         });
+    });
+
+    // Add scroll animations to reveal content
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
     });
 });
